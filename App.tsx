1196d2da752dcb5aa2c5eb088714bfa8bc275bce
umairabhatti786 <110904913+umairabhatti786@users.noreply.github.com>
2024-10-08 10:56:15 +0500

    first

---
+++ App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {View, Text, Image, Platform} from 'react-native';
+
+const App = ({children, edges}: any) => {
+  return (
+    <View>
+      <Text>testReading</Text>
+
+    </View>
+
+  );
+};
+
+export default App;
